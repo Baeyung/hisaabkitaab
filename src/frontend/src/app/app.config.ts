7ff248c9044dbc1e828711c35df0af2b87057786
Baeyung <ahmadsheikh617@gmail.com>
2026-07-15 23:22:25 +0500

    more animations

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
@@ -9,7 +9,7 @@ import { authInterceptor } from './core/auth/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({
       theme: { preset: Aura },
